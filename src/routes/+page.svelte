4dd730fce79499783815bb170be45a6c69ab4cad
Daniel Power <me@danielpower.ca>
2024-01-13 02:20:27 -0330

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { gameState } from '$lib/game';
+	import Cell from './Cell.svelte';
+</script>
+
+<div class="absolute inset-0 flex flex-col items-center justify-center">
+	<div>
+		{#each { length: 3 } as _, i}
+			<div class="flex">
+				{#each { length: 3 } as _, j}
+					<Cell id={i * 3 + j} />
+				{/each}
+			</div>
+		{/each}
+	</div>
+
+	{#if $gameState.ctx.gameover}
+		<div class="bg-white p-4 rounded-lg shadow-lg">
+			{#if $gameState.ctx.gameover.winner === '0'}
+				⭕️ You won!
+			{:else if $gameState.ctx.gameover.winner === '1'}
+				❌ You lost!
+			{:else}
+				🤝 It's a tie!
+			{/if}
+		</div>
+	{/if}
+</div>
