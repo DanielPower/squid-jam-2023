a8c6884ddacffbd4d413cd65d26b96caf781dfde
Daniel Power <me@danielpower.ca>
2024-01-20 22:52:25 -0330

    Add a friendly little squid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
-<div class="p-8 flex justify-center">
-	<div class="flex flex-col items-center gap-2">
+<script lang="ts">
+	import squid from '$lib/assets/squid.png';
+</script>
+
+<div class="p-8 min-h-svh flex justify-center items-center">
+	<div class="flex flex-col items-center gap-5">
+		<img src={squid} alt="Logo" class="max-h-40" />
 		<h1 class="text-gray-800 text-5xl">TicTacToe</h1>
 		<form method="POST" action="/lobbies/?/new">
 			<button
